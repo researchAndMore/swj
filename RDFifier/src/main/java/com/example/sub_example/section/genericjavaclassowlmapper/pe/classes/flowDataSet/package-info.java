66045aa36055604a066cfaf18a0e4b83a0a6de0c
researchAndMore <133094528+researchAndMore@users.noreply.github.com>
2025-03-06 12:04:29 +0100

    Delete RDFifier directory

--- RDFifier/src/main/java/com/example/sub_example/section/genericjavaclassowlmapper/pe/classes/flowDataSet/package-info.java
+++
@@ -1,2 +0,0 @@
-@jakarta.xml.bind.annotation.XmlSchema(namespace = "http://lca.jrc.it/ILCD/Flow", elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.example.sub_example.section.genericjavaclassowlmapper.pe.classes.flowDataSet;
